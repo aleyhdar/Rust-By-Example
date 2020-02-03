a03654b92b9fd63fd2111f43ee8fd7adfe1cc253
tolerans <scriptkidceo@gmail.com>
2020-02-03 12:47:32 +0300

    Ex01

---
+++ rust_example_01.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello RUST World!!");
+    println!("What does the fox say!");
+
+    let x = 5 + 5;
+    println!("Is 'x' 10 or 100? x={} ",x);
+}
+
+
+
+
+
+
+
+
+
+
+
+
