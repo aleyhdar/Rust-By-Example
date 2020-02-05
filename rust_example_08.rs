91fe1f0509a68469c380d317ea48854ab3670bc3
tolerans <scriptkidceo@gmail.com>
2020-02-05 19:40:36 +0300

    Ex08-Structs

---
+++ rust_example_08.rs
@@ -0,0 +1,16 @@
+struct Color {
+    red : u8,
+    green: u8,
+    blue: u8
+}
+
+fn main () {
+    let bg =Color {red: 255, green: 70, blue: 15};
+    print_color(&bg);
+}
+fn print_color(c: &Color){
+    println!("R: {} G: {} B: {}", c.red, c.green, c.blue);
+}
+
+
+
