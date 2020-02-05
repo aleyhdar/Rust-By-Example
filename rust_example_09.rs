2b958dc408d909e3d1dd0a5bd5bd4e8f2edb3c20
tolerans <scriptkidceo@gmail.com>
2020-02-05 19:56:26 +0300

    Ex09-Arrays

---
+++ rust_example_09.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let numbers = [11,22,33,44,55,66];
+    let _numbers: [u8; 4] = [77,88,99,00];
+    let zero_vector = [0; 10];
+
+    for i in numbers.iter() {
+        println!("{} ",i);
+    }
+    for i in 0.._numbers.len() {
+        print!("{} ",_numbers[i]);
+    }
+    for i in 1..20 {
+        println!("{}",zero_vector[i]);
+    }
+}
